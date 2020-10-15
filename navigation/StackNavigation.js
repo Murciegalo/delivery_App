5f1b4b39023eb0360cb9122991d0493006ec3d6f
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-10-15 19:41:18 +0100

    react-navigation bare-bones set

---
+++ navigation/StackNavigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from '../views/Home';
+
+const Stack = createStackNavigator();
+
+export default function StackNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
