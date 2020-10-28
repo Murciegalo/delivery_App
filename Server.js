8f597a19d6116c5d469fe216a248c184c2924a2a
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-10-28 21:43:43 +0100

    server on

--- Server.js
+++ Server.js
@@ -6,3 +6,10 @@ const app = express()
 app.use(cors())
 app.use(bodyparser.urlencoded({extended:false}))
 
+app.get('/', (req, res) => {
+  res.send('First get request')
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, (req, res) => console.log('Server ON'))
+
