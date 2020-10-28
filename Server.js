47507b465cf9f2b33e78f30d8061d9947f158286
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-10-28 21:34:02 +0100

    cors

---
+++ Server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+const app = express()
+app.use(cors())
+app.use(bodyparser.urlencoded({extended:false}))
+
