a12f8d73afac03fee4168f32b9e27bc429dcb8bf
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-17 18:45:23 +0100

    UI for login almost complete

--- views/home/Home.js
+++ views/home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 export default function Home(props) {
+  const onPress = () => props.navigation.navigate('login')
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
-      <Button
-        onPress={() => props.navigation.navigate('login')} title="test"
-      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
     </View>
   );
 }
