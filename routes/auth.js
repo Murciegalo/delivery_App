e319db2e7d7da337e85ae4607142248186b17d52
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-11-19 14:54:20 +0100

    backend for login & frontend plugged

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,6 @@ let user = models.User;
 
 // Login User
 router.post('/login', async (req, res) => {
-  console.log(req.body);
   let loggedUser = await user.findOne({
     where:{
       email: req.body.email,
@@ -13,7 +12,6 @@ router.post('/login', async (req, res) => {
     }
   })
   if(loggedUser !== null){
-    console.log('REQ', loggedUser);
     return res.status(200).send(loggedUser)
   }
   return res.status(404).json({
