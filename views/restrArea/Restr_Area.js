f0b8eac50eb1f47a848e0072fb01912e16b2900f
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-12-02 19:47:09 +0100

    Tab Navigation styles applied

--- views/restrArea/Restr_Area.js
+++ views/restrArea/Restr_Area.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
 import Delivery from '../delivery/Delivery'
 import EditDelivery from '../editDelivery/EditDelivery'
 import Profile from '../profile/Profile'
@@ -22,10 +23,37 @@ export default function Restr_Area() {
     getUser()
   }
   , [])
-  return <Tab.Navigator>
-    <Tab.Screen name="Profile" component={Profile} />
-    <Tab.Screen name="Deliveries" component={Delivery} />
-    <Tab.Screen name="Changes" component={EditDelivery} />
+  return <Tab.Navigator
+    activeColor="#999"
+    inactiveColor="#fff"
+    barStyle={{
+      backgroundColor: '#993',
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#333'
+    }}
+  >
+    <Tab.Screen 
+      name="Profile" 
+      component={Profile} 
+      options={{
+        tabBarIcon: () => <Icon name="users" size={20} color="#999" />
+      }}
+    />
+    <Tab.Screen 
+      name="Deliveries" 
+      component={Delivery} 
+      options={{
+        tabBarIcon: () => <Icon name="archive" size={20} color="#999" />
+      }}
+    />
+    <Tab.Screen 
+      name="Changes" 
+      component={EditDelivery} 
+      options={{
+        tabBarIcon: () => <Icon name="edit" size={20} color="#999" />
+      }}
+    />
 </Tab.Navigator>
 
 }
